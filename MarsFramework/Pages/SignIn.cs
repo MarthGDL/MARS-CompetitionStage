280cfa0f2f59f91fdfac1b57070443ae48ead9a6
Damart <limbo12_@hotmail.com>
2020-09-16 13:59:11 -0500

    Project Baseline

---
+++ MarsFramework/Pages/SignIn.cs
@@ -0,0 +1,41 @@
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace MarsFramework.Pages
+{
+    class SignIn
+    {
+        public SignIn()
+        {
+            PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
+        }
+
+        #region  Initialize Web Elements 
+        //Finding the Sign Link
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Sign')]")]
+        private IWebElement SignIntab { get; set; }
+
+        // Finding the Email Field
+        [FindsBy(How = How.Name, Using = "email")]
+        private IWebElement Email { get; set; }
+
+        //Finding the Password Field
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement Password { get; set; }
+
+        //Finding the Login Button
+        [FindsBy(How = How.XPath, Using = "//button[contains(text(),'Login')]")]
+        private IWebElement LoginBtn { get; set; }
+
+        #endregion
+
+        internal void LoginSteps()
+        {
+            //Populate the excel data
+            GlobalDefinitions.ExcelLib.PopulateInCollection(Base.ExcelPath, "SignIn");
+            //Click on Join button
+            
+        }
+    }
+}
---
+++ MarsFramework/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
+using System.Threading;
+
+namespace MarsFramework
+{
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+
+            [Test]
+            public void Test()
+            {
+                SignUp SignUpPage = new SignUp();
+                SignUpPage.register();
+
+            }
+
+
+
+        }
+    }
+}
